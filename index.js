c0efd610f79f7ef1fc3217a975109b48b4dcd0d0
Valter Lima Mesquita <limavalter@gmail.com>
2016-02-22 14:41:23 -0300

    Fixed return

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var options = {
-	moviesDir: 'files/movies/',
-	subtitlesDir: 'files/subtitles/',
-	outputDir: 'files/output/',
-	renameSubtitle: true,
-	moveFiles: false
+  moviesDir: 'files/movies/',
+  subtitlesDir: 'files/subtitles/',
+  outputDir: 'files/output/',
+  renameSubtitle: true,
+  moveFiles: false
 }
 var Matcher = require('./lib/subtitle-matcher.js');
 
